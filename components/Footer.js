2e5eadb506f5926a46a4bcd103a3020d37103691
Bhuvangoyal466 <bhuvangoyal6002@gmail.com>
2025-07-08 19:19:30 +0530

    displayed the T shirts by data from MongoDB

--- components/Footer.js
+++ components/Footer.js
@@ -151,9 +151,9 @@ const Footer = () => {
                             <a className="text-gray-500">
                                 <svg
                                     fill="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
                                     className="w-5 h-5"
                                     viewBox="0 0 24 24"
                                 >
@@ -163,9 +163,9 @@ const Footer = () => {
                             <a className="ml-3 text-gray-500">
                                 <svg
                                     fill="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
                                     className="w-5 h-5"
                                     viewBox="0 0 24 24"
                                 >
@@ -176,9 +176,9 @@ const Footer = () => {
                                 <svg
                                     fill="none"
                                     stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
                                     className="w-5 h-5"
                                     viewBox="0 0 24 24"
                                 >
@@ -197,9 +197,9 @@ const Footer = () => {
                                 <svg
                                     fill="currentColor"
                                     stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="0"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="0"
                                     className="w-5 h-5"
                                     viewBox="0 0 24 24"
                                 >
--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,201 +1,60 @@
 import React from "react";
 import Link from "next/link";
+import Product from "../models/Product";
+import mongoose from "mongoose";
 
-const Tshirts = () => {
+const Tshirts = ({ products }) => {
     return (
         <div>
-            <section className="text-gray-600 body-font ml-20 mr-20">
+            <section className="text-gray-600 body-font">
                 <div className="container px-30 py-24 mx-auto">
-                    <div className="flex flex-wrap -m-4">
-                        <Link href={"/product/allen-solly"} legacyBehavior>
-                            <a className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg mb-10">
-                                <div>
-                                    <a className="block relative h-48 rounded overflow-hidden">
-                                        <img
-                                            alt="ecommerce"
-                                            className="m-auto md:m-0 object-cover object-center w-full h-full block"
-                                            src="https://m.media-amazon.com/images/I/61-BZ6fzn6L._SX569_.jpg"
-                                        />
-                                    </a>
-                                    <div className="mt-4 text-center">
-                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                            T-Shirt
-                                        </h3>
-                                        <h2 className="text-gray-900 title-font text-lg font-medium">
-                                            Allen Solly
-                                        </h2>
-                                        <p className="mt-1">₹959</p>
-                                        <p className="mt-1">S,M,L,XL</p>
+                    <div className="flex flex-wrap -m-4 justify-center">
+                        {products.map((product) => (
+                            <Link
+                                href={`/product/${product.slug}`}
+                                legacyBehavior
+                                key={product._id}
+                            >
+                                <a className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg mb-10">
+                                    <div>
+                                        <div className="block relative h-48 rounded overflow-hidden">
+                                            <img
+                                                alt="ecommerce"
+                                                className="m-auto md:m-0 object-cover object-center w-full h-full block"
+                                                src={product.img}
+                                            />
+                                        </div>
+                                        <div className="mt-4 text-center">
+                                            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                                                T-Shirt
+                                            </h3>
+                                            <h2 className="text-gray-900 title-font text-lg font-medium">
+                                                {product.title}
+                                            </h2>
+                                            <p className="mt-1">
+                                                ₹{product.price}
+                                            </p>
+                                            <p className="mt-1">S,M,L,XL</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </a>
-                        </Link>
-                        <Link href={"/product/allen-solly"} legacyBehavior>
-                            <a className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg mb-10">
-                                <div>
-                                    <a className="block relative h-48 rounded overflow-hidden">
-                                        <img
-                                            alt="ecommerce"
-                                            className="m-auto md:m-0 object-cover object-center w-full h-full block"
-                                            src="https://m.media-amazon.com/images/I/61-BZ6fzn6L._SX569_.jpg"
-                                        />
-                                    </a>
-                                    <div className="mt-4 text-center">
-                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                            T-Shirt
-                                        </h3>
-                                        <h2 className="text-gray-900 title-font text-lg font-medium">
-                                            Allen Solly
-                                        </h2>
-                                        <p className="mt-1">₹959</p>
-                                        <p className="mt-1">S,M,L,XL</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </Link>
-                        <Link href={"/product/allen-solly"} legacyBehavior>
-                            <a className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg mb-10">
-                                <div>
-                                    <a className="block relative h-48 rounded overflow-hidden">
-                                        <img
-                                            alt="ecommerce"
-                                            className="m-auto md:m-0 object-cover object-center w-full h-full block"
-                                            src="https://m.media-amazon.com/images/I/61-BZ6fzn6L._SX569_.jpg"
-                                        />
-                                    </a>
-                                    <div className="mt-4 text-center">
-                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                            T-Shirt
-                                        </h3>
-                                        <h2 className="text-gray-900 title-font text-lg font-medium">
-                                            Allen Solly
-                                        </h2>
-                                        <p className="mt-1">₹959</p>
-                                        <p className="mt-1">S,M,L,XL</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </Link>
-                        <Link href={"/product/allen-solly"} legacyBehavior>
-                            <a className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg mb-10">
-                                <div>
-                                    <a className="block relative h-48 rounded overflow-hidden">
-                                        <img
-                                            alt="ecommerce"
-                                            className="m-auto md:m-0 object-cover object-center w-full h-full block"
-                                            src="https://m.media-amazon.com/images/I/61-BZ6fzn6L._SX569_.jpg"
-                                        />
-                                    </a>
-                                    <div className="mt-4 text-center">
-                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                            T-Shirt
-                                        </h3>
-                                        <h2 className="text-gray-900 title-font text-lg font-medium">
-                                            Allen Solly
-                                        </h2>
-                                        <p className="mt-1">₹959</p>
-                                        <p className="mt-1">S,M,L,XL</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </Link>
-                        <Link href={"/product/allen-solly"} legacyBehavior>
-                            <a className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg mb-10">
-                                <div>
-                                    <a className="block relative h-48 rounded overflow-hidden">
-                                        <img
-                                            alt="ecommerce"
-                                            className="m-auto md:m-0 object-cover object-center w-full h-full block"
-                                            src="https://m.media-amazon.com/images/I/61-BZ6fzn6L._SX569_.jpg"
-                                        />
-                                    </a>
-                                    <div className="mt-4 text-center">
-                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                            T-Shirt
-                                        </h3>
-                                        <h2 className="text-gray-900 title-font text-lg font-medium">
-                                            Allen Solly
-                                        </h2>
-                                        <p className="mt-1">₹959</p>
-                                        <p className="mt-1">S,M,L,XL</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </Link>
-                        <Link href={"/product/allen-solly"} legacyBehavior>
-                            <a className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg mb-10">
-                                <div>
-                                    <a className="block relative h-48 rounded overflow-hidden">
-                                        <img
-                                            alt="ecommerce"
-                                            className="m-auto md:m-0 object-cover object-center w-full h-full block"
-                                            src="https://m.media-amazon.com/images/I/61-BZ6fzn6L._SX569_.jpg"
-                                        />
-                                    </a>
-                                    <div className="mt-4 text-center">
-                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                            T-Shirt
-                                        </h3>
-                                        <h2 className="text-gray-900 title-font text-lg font-medium">
-                                            Allen Solly
-                                        </h2>
-                                        <p className="mt-1">₹959</p>
-                                        <p className="mt-1">S,M,L,XL</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </Link>
-                        <Link href={"/product/allen-solly"} legacyBehavior>
-                            <a className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg mb-10">
-                                <div>
-                                    <a className="block relative h-48 rounded overflow-hidden">
-                                        <img
-                                            alt="ecommerce"
-                                            className="m-auto md:m-0 object-cover object-center w-full h-full block"
-                                            src="https://m.media-amazon.com/images/I/61-BZ6fzn6L._SX569_.jpg"
-                                        />
-                                    </a>
-                                    <div className="mt-4 text-center">
-                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                            T-Shirt
-                                        </h3>
-                                        <h2 className="text-gray-900 title-font text-lg font-medium">
-                                            Allen Solly
-                                        </h2>
-                                        <p className="mt-1">₹959</p>
-                                        <p className="mt-1">S,M,L,XL</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </Link>
-                        <Link href={"/product/allen-solly"} legacyBehavior>
-                            <a className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg mb-10 ">
-                                <div>
-                                    <a className="block relative h-48 rounded overflow-hidden">
-                                        <img
-                                            alt="ecommerce"
-                                            className="m-auto md:m-0 object-cover object-center w-full h-full block"
-                                            src="https://m.media-amazon.com/images/I/61-BZ6fzn6L._SX569_.jpg"
-                                        />
-                                    </a>
-                                    <div className="mt-4 text-center">
-                                        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                                            T-Shirt
-                                        </h3>
-                                        <h2 className="text-gray-900 title-font text-lg font-medium">
-                                            Allen Solly
-                                        </h2>
-                                        <p className="mt-1">₹959</p>
-                                        <p className="mt-1">S,M,L,XL</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </Link>
+                                </a>
+                            </Link>
+                        ))}
                     </div>
                 </div>
             </section>
         </div>
     );
 };
+
+export async function getServerSideProps() {
+    if (!mongoose.connections[0].readyState) {
+        await mongoose.connect(process.env.MONGO_URI);
+    }
+    let products = await Product.find({ category: "tshirt" });
+    return {
+        props: { products: JSON.parse(JSON.stringify(products)) },
+    };
+}
 
 export default Tshirts;
