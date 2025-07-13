ae11dffa2569e302f3f258ba6e2fc883f4daaf77
Bhuvangoyal466 <bhuvangoyal6002@gmail.com>
2025-07-13 14:57:57 +0530

    Created orders page

---
+++ pages/myaccount.js
@@ -0,0 +1,16 @@
+import React, { use } from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const MyAccount = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!localStorage.getItem("token")) {
+            router.push("/");
+        }
+    }, []);
+    return <div>My Account</div>;
+};
+
+export default MyAccount;
